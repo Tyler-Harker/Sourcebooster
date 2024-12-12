d39a2d3d533f5b9c9e93af95f8fc809df8d0e474
Tyler Harker <me@tylerharker.com>
2024-12-12 13:19:04 -0700

    chore: initial commit

---
+++ Frontend/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAkYsWV95h5UHAWxRYCQMH5Tu7sIz1xaCs",
+    authDomain: "sourcebooster.firebaseapp.com",
+    projectId: "sourcebooster",
+    storageBucket: "sourcebooster.firebasestorage.app",
+    messagingSenderId: "912553701887",
+    appId: "1:912553701887:web:83750044892b34ca80b7ca",
+    measurementId: "G-KGF4QERK8S"
+};
+
+export default firebaseConfig;
