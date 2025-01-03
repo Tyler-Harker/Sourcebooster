cf840e65cad53001dff3fee3ce17ab6455b0cf08
Tyler Harker <me@tylerharker.com>
2025-01-03 15:01:52 -0700

    fix: renamed type

--- Frontend/components/authContext/authContext.server.tsx
+++ Frontend/components/authContext/authContext.server.tsx
@@ -2,18 +2,18 @@ import { ReactNode } from "react"
 import { AuthContext } from "./authContext"
 import { cookies } from "next/headers";
 import { jwtDecode } from "jwt-decode";
-import IUser from "../../../Models/user";
+import User from "../../../Models/user";
 
 export const AuthContextServer = async ({ children }: { children: ReactNode }) => {
     const cookieStore = await cookies();
     const token = cookieStore.get('token')?.value;
-    let user: IUser | undefined = undefined;
+    let user: User | undefined = undefined;
     if (token) {
         const decodedToken = jwtDecode(token);
         user = {
             id: decodedToken.sub,
             email: 'email'
-        } as IUser
+        } as User
     }
 
     return <AuthContext initialUser={user}>
